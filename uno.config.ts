bec1fa87ce715aabf33fc71b2d37e865930b3eb2
codefug <robot9917@gmail.com>
2026-05-09 15:44:54 +0900

    chore: biome 포맷 정리 (config 파일)

--- uno.config.ts
+++ uno.config.ts
@@ -1,8 +1,8 @@
-import { defineConfig, presetUno, presetAttributify } from 'unocss'
+import { defineConfig, presetAttributify, presetUno } from 'unocss'
 
 export default defineConfig({
   presets: [
-    presetUno(),       // Tailwind/Windi CSS 호환 유틸리티 클래스
+    presetUno(), // Tailwind/Windi CSS 호환 유틸리티 클래스
     presetAttributify() // <div text-sm font-bold> 방식도 가능
   ]
 })
