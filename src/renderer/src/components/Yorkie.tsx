84d60bcb64d642cc9e509bf52d4b5bd4310f7064
codefug <robot9917@gmail.com>
2026-05-09 11:25:34 +0900

    feat(character): yorkie aborted pose svg

--- src/renderer/src/components/Yorkie.tsx
+++ src/renderer/src/components/Yorkie.tsx
@@ -2,18 +2,13 @@ import type { SessionStatus } from '../types'
 import workingSvg from '../assets/yorkie/working.svg'
 import waitingSvg from '../assets/yorkie/waiting.svg'
 import doneSvg from '../assets/yorkie/done.svg'
+import abortedSvg from '../assets/yorkie/aborted.svg'
 
-const SVG_MAP: Partial<Record<SessionStatus, string>> = {
+const SVG_MAP: Record<SessionStatus, string> = {
   working: workingSvg,
   waiting_permission: waitingSvg,
-  done: doneSvg
-}
-
-const PLACEHOLDER_COLORS: Record<SessionStatus, string> = {
-  working: '#F5C842',
-  waiting_permission: '#F5813A',
-  done: '#4CAF7D',
-  aborted: '#888899'
+  done: doneSvg,
+  aborted: abortedSvg
 }
 
 interface Props {
@@ -21,21 +16,5 @@ interface Props {
 }
 
 export default function Yorkie({ status }: Props): React.JSX.Element {
-  const svg = SVG_MAP[status]
-  if (svg) {
-    return <img src={svg} width={48} height={48} style={{ display: 'block' }} />
-  }
-
-  return (
-    <div
-      style={{
-        width: 48,
-        height: 48,
-        borderRadius: '50%',
-        background: PLACEHOLDER_COLORS[status],
-        opacity: 0.3,
-        flexShrink: 0
-      }}
-    />
-  )
+  return <img src={SVG_MAP[status]} width={48} height={48} style={{ display: 'block' }} />
 }
